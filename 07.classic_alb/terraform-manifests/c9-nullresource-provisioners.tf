222a7b1b9bc78994fe71ae5b6cde8adfdf91c56f
Mukesh441 <sethi.mukeshjain@gmail.com>
2025-08-29 04:54:51 +0530

    Welcome to StackSimplify

---
+++ 07.classic_alb/terraform-manifests/c9-nullresource-provisioners.tf
@@ -0,0 +1,39 @@
+# Create a Null Resource and Provisioners
+resource "null_resource" "name" {
+  depends_on = [module.ec2_public]
+  # Connection Block for Provisioners to connect to EC2 Instance
+  connection {
+    type     = "ssh"
+    host     = aws_eip.bastion_eip.public_ip    
+    user     = "ec2-user"
+    password = ""
+    private_key = file("private-key/us_virginia_key.pem")
+  }  
+
+## File Provisioner: Copies the us_virginia_key.pem file to /tmp/us_virginia_key.pem
+  provisioner "file" {
+    source      = "private-key/us_virginia_key.pem"
+    destination = "/tmp/us_virginia_key.pem"
+    on_failure = continue
+  }
+## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 400 /tmp/us_virginia_key.pem"
+    ]
+    on_failure = continue
+  }
+
+## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
+/*  provisioner "local-exec" {
+    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    working_dir = "local-exec-output-files/"
+    when = destroy
+    #on_failure = continue
+  }  
+  */
+
+}
+
+# Creation Time Provisioners - By default they are created during resource creations (terraform apply)
+# Destory Time Provisioners - Will be executed during "terraform destroy" command (when = destroy)
