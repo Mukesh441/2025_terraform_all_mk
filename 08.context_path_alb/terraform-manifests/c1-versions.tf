6bdb7a40141e084af5ec5cca717c23d97b5be1b8
Mukesh441 <sethi.mukeshjain@gmail.com>
2025-08-29 11:51:40 +0530

    Welcome to StackSimplify

---
+++ 08.context_path_alb/terraform-manifests/c1-versions.tf
@@ -0,0 +1,26 @@
+# Terraform Settings Block
+terraform {
+  backend "s3" {
+    bucket = "mk-terraform-state-cicd"   # Your bucket name (lowercase)
+    key    = "dev/context_path_alb/terraform.tfstate" # State file path (e.g., "terraform.tfstate" or in folder)
+    region = "us-east-1"  
+    dynamodb_table = "terraform-eks-state-locks"                # Your bucket region
+    # use_lockfile = true  # For state locking with DynamoDB (optional but recommended)
+    encrypt = true                        # Enable encryption of the state file
+  }
+  required_version = ">=  1.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # Optional but recommended in production
+    }
+
+  }
+}
+
+
+# Provider Block
+provider "aws" {
+  # profile = var.profile    //"default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = var.aws_region //"ap-south-1"
+}
